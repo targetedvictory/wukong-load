44f5f290fc77cf72c7b5a1e5898280676ce8dd71
Eric Li <eli@targetedvictory.com>
2016-02-01 12:10:50 -0500

    Remove pry

--- lib/wukong-load/syncers/prepare_syncer/datorama_ordered_handler.rb
+++ lib/wukong-load/syncers/prepare_syncer/datorama_ordered_handler.rb
@@ -14,7 +14,6 @@ module Wukong
         # @param [Pathname] original
         # @return [Pathname]
         def path_for original
-          binding.pry
           current_output_directory.join(daily_directory_for(file_time(original).strftime(settings[:ordered_time_pattern]), original)).join(File.mtime(original.to_path).to_i.to_s).join(slug_for(processing_time, original))
         end
       end
